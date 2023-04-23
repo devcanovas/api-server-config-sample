dce6b7ce29927b18e931e498dc2c18bb383401d3
Matheus Canovas <matheuscanovas9@gmail.com>
2023-04-23 14:09:56 -0300

    Creatin project

---
+++ src/main/java/com/github/omtcnvs/api/server/config/sample/ApiServerConfigSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.omtcnvs.api.server.config.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerConfigSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServerConfigSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/omtcnvs/api/server/config/sample/ApiServerConfigSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.omtcnvs.api.server.config.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerConfigSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
